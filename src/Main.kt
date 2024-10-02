d1ef820723d05434f85057665f9d27c073f3ac5e
Admin <reteamboss@gmail.com>
2024-10-02 16:34:56 +0300

    3

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,24 @@
+import java.io.File
+import java.io.FileReader
+import java.io.FileWriter
+
 fun main() {
-    println("Hello World!")
+
+    val file = File("C:/Users/Админ/Desktop/myfile.txt")
+    file.writeFile("С новым годом: С новым счастьем")
+    readFile(file.toString())
+}
+
+infix fun File.writeFile(text: String){
+    val writer = FileWriter(this)
+    writer.write(text)
+    writer.close()
+}
+
+fun readFile(path: String){
+
+    val reader = FileReader(path)
+    val content = reader.readText()
+    println(content)
+    reader.close()
 }
